ba2c11735e36f10acca0f7b8156141e6e8d23266
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-19 00:13:40 +0300

    Stage 5: test Turkish chord readings

---
+++ test/chord-labels-tr.test.js
@@ -0,0 +1,13 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+import { chordNameTr } from "../src/chord-labels-tr.js";
+import { parseChordQuery } from "../src/chord-core.js";
+
+test("renders required Turkish chord readings", () => {
+  assert.equal(chordNameTr(parseChordQuery("Am")), "La minör");
+  assert.equal(chordNameTr(parseChordQuery("E")), "Mi majör");
+  assert.equal(chordNameTr(parseChordQuery("G7")), "Sol yedili");
+  assert.equal(chordNameTr(parseChordQuery("F#maj7")), "Fa diyez majör yedili");
+  assert.equal(chordNameTr(parseChordQuery("Bm7")), "Si minör yedili");
+  assert.equal(chordNameTr(parseChordQuery("Dsus4")), "Re sus dört");
+});
