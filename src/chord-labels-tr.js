eeaacee88d960a2b262ce0ede84a28bc746a634d
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-19 01:03:52 +0300

    Stage 14: add Turkish flat chord readings

--- src/chord-labels-tr.js
+++ src/chord-labels-tr.js
@@ -1,16 +1,25 @@
 const ROOT_NAMES_TR = Object.freeze({
   C:"Do",
   "C#":"Do diyez",
+  Db:"Re bemol",
   D:"Re",
   "D#":"Re diyez",
+  Eb:"Mi bemol",
   E:"Mi",
+  Fb:"Fa bemol",
+  "E#":"Mi diyez",
   F:"Fa",
   "F#":"Fa diyez",
+  Gb:"Sol bemol",
   G:"Sol",
   "G#":"Sol diyez",
+  Ab:"La bemol",
   A:"La",
   "A#":"La diyez",
-  B:"Si"
+  Bb:"Si bemol",
+  B:"Si",
+  Cb:"Do bemol",
+  "B#":"Si diyez"
 });
 
 const QUALITY_NAMES_TR = Object.freeze({
@@ -26,7 +35,7 @@ const QUALITY_NAMES_TR = Object.freeze({
 
 export function chordNameTr(chord) {
   if (!chord || typeof chord !== "object") throw new TypeError("chord is required");
-  const root = ROOT_NAMES_TR[chord.root];
+  const root = ROOT_NAMES_TR[chord.displayRoot || chord.root];
   const quality = QUALITY_NAMES_TR[chord.quality];
   if (!root || !quality) throw new Error("Unsupported chord identity");
   return `${root} ${quality}`;
