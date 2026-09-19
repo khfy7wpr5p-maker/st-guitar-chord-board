792bf91bd575a24080e37d129056afa26ce64291
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-09-19 10:04:58 +0300

    Stage 22: enforce index-barre-only fingering policy
    Apply the index-barre-only fingering policy across all displayed voicings while preserving the 276-voicing catalog contract.

--- src/voicing-library.js
+++ src/voicing-library.js
@@ -8,7 +8,7 @@ const V = (frets, fingers, barres = [], shape = "curated") => ({ frets, fingers,
 export const C_FAMILY_VOICINGS = Object.freeze({
   C: [
     V([-1,3,2,0,1,0],[-1,3,2,0,1,0],[],"open"),
-    V([-1,3,5,5,5,3],[-1,1,3,3,3,1],[{finger:1,fret:3,fromString:5,toString:1},{finger:3,fret:5,fromString:4,toString:2}],"A"),
+    V([-1,3,5,5,5,3],[-1,1,2,3,4,1],[{finger:1,fret:3,fromString:5,toString:1}],"A"),
     V([8,10,10,9,8,8],[1,3,4,2,1,1],[{finger:1,fret:8,fromString:6,toString:1}],"E")
   ],
   Cm: [
@@ -17,7 +17,7 @@ export const C_FAMILY_VOICINGS = Object.freeze({
     V([8,10,10,8,8,8],[1,3,4,1,1,1],[{finger:1,fret:8,fromString:6,toString:1}],"Em")
   ],
   C7: [
-    V([-1,3,2,3,5,3],[-1,2,1,3,4,3],[{finger:3,fret:3,fromString:3,toString:1}],"complete-open-region"),
+    V([-1,3,5,3,5,6],[-1,1,2,1,3,4],[{finger:1,fret:3,fromString:5,toString:3}],"open-region"),
     V([-1,3,5,3,5,3],[-1,1,3,1,4,1],[{finger:1,fret:3,fromString:5,toString:1}],"A7"),
     V([8,10,8,9,8,8],[1,3,1,2,1,1],[{finger:1,fret:8,fromString:6,toString:1}],"E7")
   ],
@@ -34,12 +34,12 @@ export const C_FAMILY_VOICINGS = Object.freeze({
   Csus2: [
     V([-1,3,0,0,1,3],[-1,2,0,0,1,4],[],"open"),
     V([-1,3,5,5,3,3],[-1,1,3,4,1,1],[{finger:1,fret:3,fromString:5,toString:1}],"Asus2"),
-    V([8,10,10,7,8,8],[2,4,4,1,3,3],[{finger:4,fret:10,fromString:5,toString:4},{finger:3,fret:8,fromString:2,toString:1}],"Esus2")
+    V([8,10,10,7,-1,-1],[2,3,4,1,-1,-1],[],"Esus2")
   ],
   Csus4: [
     V([-1,3,3,0,1,1],[-1,3,4,0,1,1],[{finger:1,fret:1,fromString:2,toString:1}],"open"),
-    V([-1,3,5,5,6,3],[-1,1,3,3,4,1],[{finger:1,fret:3,fromString:5,toString:1},{finger:3,fret:5,fromString:4,toString:3}],"Asus4"),
-    V([8,10,10,10,8,8],[1,3,4,4,1,1],[{finger:1,fret:8,fromString:6,toString:1},{finger:4,fret:10,fromString:4,toString:3}],"Esus4")
+    V([-1,3,5,5,6,3],[-1,1,2,3,4,1],[{finger:1,fret:3,fromString:5,toString:1}],"Asus4"),
+    V([8,10,10,10,8,8],[1,2,3,4,1,1],[{finger:1,fret:8,fromString:6,toString:1}],"Esus4")
   ]
 });
 
